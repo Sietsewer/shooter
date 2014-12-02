c47abb4fdaa4d91bff44990c64095ae9b0dc93fd
Sietsewer <sietsewer@gmail.com>
2014-12-02 23:06:33 +0100

    Added project

---
+++ Shooter/Assets/Flashlight.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flashlight : MonoBehaviour {
+
+	// Use this for initialization
+	public Keybinds keybinds;
+	public bool lightOn = true;
+	public GameObject aimPoint;
+	public GameObject startPoint;
+	public GameObject pointLight;
+	private float closeDist;
+	void Start () {
+		closeDist = Vector3.Distance(this.transform.position, startPoint.transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		RaycastHit hitClose;
+		Physics.Linecast(startPoint.transform.position, aimPoint.transform.position, out hit);
+		Physics.Linecast(this.transform.position, aimPoint.transform.position, out hitClose);
+		if(hit.collider != null){
+			pointLight.transform.position = hit.point;
+		} else {
+			if(hitClose.collider != null){
+				pointLight.transform.position = startPoint.transform.position;
+			} else {
+				pointLight.transform.position = aimPoint.transform.position;
+			}
+		}
+		if(Input.GetKeyDown(Keybinds.flashLight)){
+			if(lightOn){
+				this.light.enabled = false;
+				this.transform.FindChild("PointLight").FindChild("Point light").light.enabled = false;
+
+				lightOn = false;
+			} else {
+				this.light.enabled = true;
+				this.transform.FindChild("PointLight").FindChild("Point light").light.enabled = true;
+
+				lightOn = true;
+			}
+		}
+	}
+}
---
+++ Shooter/Assets/Keybinds.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Keybinds : MonoBehaviour{
+	public static KeyCode flashLight;
+	public static KeyCode walkForward;
+	public static KeyCode walkBackward;
+	public static KeyCode walkRight;
+	public static KeyCode walkLeft;
+	public static KeyCode jump;
+
+	void Start(){
+		flashLight	 = KeyCode.F;
+		walkForward	 = KeyCode.W;
+		walkBackward = KeyCode.S;
+		walkRight	 = KeyCode.A;
+		walkLeft	 = KeyCode.D;
+		jump		 = KeyCode.Space;
+	}
+}
